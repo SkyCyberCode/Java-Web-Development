8a8c0619cd25e2493a490731e6c59a42dfde4c52
Sky <191615983+SkyCyberCode@users.noreply.github.com>
2025-09-21 22:16:06 +0300

    Update AddIntegers.java

--- Week_1/Labs/Simple_console_integer_calculator/AddIntegers.java
+++ Week_1/Labs/Simple_console_integer_calculator/AddIntegers.java
@@ -1,32 +1,34 @@
 /*
-Implement console program which meets the following requirements:
+Implement console program which meet the following requirements:
 a- Program is started with 2 input arguments.
 
 b- Program parses arguments to Integer.
-   Example: int i = Integer.parseInt("1");
+To parse String arguments to int, consider the next example:
+int i = Integer.parseInt(“1”);
 
-c- Program prints sum of integers to console.
+c- Program prints sum of integers to console
 */
 
 public class AddIntegers {
 
     public static void main(String[] args) {
-        System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
-    }
+		System.out.println(Integer.parseInt(args[0])+Integer.parseInt(args[1]));
+	}
 
 }
 
 /*
 Explanation:
-1- In the main method, we receive an array called "args". This array stores the input arguments 
-   that the user provides when running the program.
+  1- In Main method.. it has an empty array called "args", We can make user give us an input, We will use it as input method.
+    ~ The user will give us the inputs after Compilation Process.
+    ```Terminal
+          javac AddIntegers.java
+          java AddIntegers 3 5
+    ```
+    
+  2- The input data type will be "String" and the program want us to calculate numbers. so, We will use "Integer.parseInt();" Method to convert from "Strings" to "Integers".
+  
+  At the end, We will print the result by using "System.out.println();".
 
-   Example of running the program with inputs:
-       javac AddIntegers.java
-       java AddIntegers 3 5
-
-2- Since the input arguments are Strings by default, we need to convert them into integers. 
-   To do this, we use the method Integer.parseInt().
-
-3- Finally, we add the two integers and print the result using System.out.println().
+  📌 Note: If you run the program without passing 2 arguments, it will throw an ArrayIndexOutOfBoundsException.
 */
