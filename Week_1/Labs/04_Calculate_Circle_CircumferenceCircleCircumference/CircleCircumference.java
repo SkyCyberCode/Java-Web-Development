4b856514ca55123e664aa2fea472e4229c89dff2
Sky <191615983+SkyCyberCode@users.noreply.github.com>
2025-09-21 23:19:28 +0300

    Update CircleCircumference.java

--- Week_1/Labs/04_Calculate_Circle_CircumferenceCircleCircumference/CircleCircumference.java
+++ Week_1/Labs/04_Calculate_Circle_CircumferenceCircleCircumference/CircleCircumference.java
@@ -6,21 +6,22 @@ b- Program calculates circle circumference
 c- Program prints circle circumference to console
 
 */
-//Java
-  import java.util.Scanner;
 
-public class CircleCircumference {
-    
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Please, enter circle radius: ");
-		double radius = sc.nextDouble();
-		
-		double circleCircumference = 2 * Math.PI * radius;
-		System.out.println("Circle circumference is: " + circleCircumference);
+//Java
+	import java.util.Scanner;
+	
+	public class CircleCircumference {
+	    
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Please, enter circle radius: ");
+			double radius = sc.nextDouble();
+			
+			double circleCircumference = 2 * Math.PI * radius;
+			System.out.println("Circle circumference is: " + circleCircumference);
+		}
+	    
 	}
-    
-}
 
 /*
 Explanation:
